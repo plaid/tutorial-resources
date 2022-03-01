2fa9c2912ec8c620c6490c4d979b0ca81a2b9371
Todd Kerpelman <tkerpelman@plaid.com>
2022-03-01 15:04:14 -0800

    Minor logging changes

--- vanilla-js-oauth/start/server.js
+++ vanilla-js-oauth/start/server.js
@@ -61,7 +61,7 @@ app.get("/api/get_bank_name", async (req, res, next) => {
     country_codes: ["US"],
   };
   const instResponse = await client.institutionsGetById(configs);
-  // console.log(`Institution Info: ${JSON.stringify(instResponse.data)}`);
+  console.log(`Institution Info: ${JSON.stringify(instResponse.data)}`);
   const bankName = instResponse.data.institution.name;
   res.json({ name: bankName });
 });
@@ -106,10 +106,6 @@ app.get("/api/transactions", async (req, res, next) => {
     end_date: endDate,
     options: { count: 10 },
   });
-  console.log(
-    `Transaction response: ${JSON.stringify(transactionResponse.data)}`
-  );
-
   res.json(transactionResponse.data);
 });
 
